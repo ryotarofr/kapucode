acd94e44a390e4331fff8e219e27cf1a058a6a08
ryotarofr <ryoryo.fr0608@icloud.com>
2023-04-04 08:56:48 +0900

    ➕ sandbox editor

--- components/content/MonacoEditar.tsx
+++ components/content/MonacoEditar.tsx
@@ -27,34 +27,92 @@
 
 // export default CodeEditor;
 
-import { Sandpack } from "@codesandbox/sandpack-react";
+import {
+  Sandpack,
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+} from "@codesandbox/sandpack-react";
+import { amethyst } from "@codesandbox/sandpack-themes";
+import { useState } from "react";
 
 export default function CodeEditor() {
+  const [files, setFiles] = useState({
+    "/button.js": {
+      code: `import ReactMarkdown from 'react-markdown';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+      
+export default function Button() {
+    return (
+      <h1>
+        aaaaaasasasa
+       </h1>
+    )
+    }`,
+    },
+  });
+  // useEffect(() => {
+  //   setFiles((prevFiles) => {
+  //     return {
+  //       ...prevFiles,
+  //       '/button.js': {
+  //         code: 'console.log("Hello World!");',
+  //       },
+  //     };
+  //   });
+  // }, []);
   return (
-    <Sandpack
-      customSetup={{
-        dependencies: {
-          "react-markdown": "latest",
-        },
-      }}
-      files={{
-        "/button.js": `import ReactMarkdown from 'react-markdown' 
+    <>
+      <Sandpack
+        customSetup={{
+          dependencies: {
+            "react-markdown": "latest",
+          },
+        }}
+        files={files}
+        // options={{
+        //   activeFile: "/App.js",
+        //   externalResources: ["https://cdn.tailwindcss.com"],
+        //   visibleFiles: ["/App.js", "/button.js", "/index.js"],
+        // }}
 
-export default function App() {
+        template="nextjs"
+        theme={"auto"}
+      />
+    </>
+  );
+}
+
+type Files = {
+  files: any;
+};
+
+export const TestB = ({ files }: Files) => (
+  <SandpackProvider files={files} template="nextjs" theme={amethyst}>
+    <SandpackLayout>
+      {/* <SandpackFileExplorer /> */}
+      <SandpackCodeEditor style={{ height: "80vh" }} />
+      <SandpackPreview style={{ height: "80vh" }} />
+    </SandpackLayout>
+  </SandpackProvider>
+);
+
+export function TestEditor() {
+  const [files, setFiles] = useState({
+    "index.html": {
+      code: "<div>Hello World!</div>",
+    },
+    "index.js": {
+      code: 'console.log("Hello World!");',
+    },
+  });
+
   return (
-    <h1>
-      aaaaaasasasa
-    </h1>
-  )
-}`,
-      }}
-      options={{
-        activeFile: "/button.js",
-        externalResources: ["https://cdn.tailwindcss.com"],
-        visibleFiles: ["/App.js", "/button.js", "/index.js"],
-      }}
-      template="react"
-      theme={"auto"}
-    />
+    <div>
+      <Sandpack files={files} />
+    </div>
   );
 }
